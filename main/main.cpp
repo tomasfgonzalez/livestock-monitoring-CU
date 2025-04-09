157d1407a55c69d4a24bcc2c770bc4d679f46b2b
brimix <only.brimix@gmail.com>
2025-04-08 22:41:59 -0300

    First refactor

--- main/main.cpp
+++ main/main.cpp
@@ -3,23 +3,34 @@
 #include <freertos/task.h>
 #include <freertos/mpu_wrappers.h>
 #include <freertos/projdefs.h>
-#include "uart0.h"
+#include "uart.h"
+#include "rylr998.h"
 
 #define BUF_SIZE 256
 
-extern "C" void app_main(void) {
-  uart0_init(BUF_SIZE);
-
-  uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
-  while (true) {
-    int len = uart0_receive(data, BUF_SIZE);
-
-    // Write data back to the UART
-    uart0_send(data, len);
-    if (len) {
-      data[len] = '\0';
-      // printf("LAPUTAMADRE! str: %s", (char *) data);
+static void rx_task(void *arg)
+{
+    static const char *RX_TASK_TAG = "RX_TASK";
+    esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+    uint8_t* data = (uint8_t*) malloc(BUF_SIZE + 1);
+    while (1) {
+        const int rxBytes = uart_read_bytes(UART_NUM_1, data, BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+        if (rxBytes > 0) {
+            data[rxBytes] = 0;
+            ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
+            ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+        }
     }
-    vTaskDelay(pdMS_TO_TICKS(1000));
+    free(data);
+}
+
+extern "C" void app_main(void) {
+  uart_init(BUF_SIZE, UART1_PORT_NUM);
+
+  xTaskCreate(rx_task, "uart_rx_task", 1024 * 2, NULL, configMAX_PRIORITIES - 1, NULL);
+
+  while (1) {
+    CU_sendConfigPackage();
+    vTaskDelay(pdMS_TO_TICKS(3000));
   }
 }
---
+++ main/rx_buff.c
@@ -0,0 +1,61 @@
+/**
+  ******************************************************************************
+  * @file           : rx_buff.c
+  * @brief          : Source for rx buffer management
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "rx_buff.h"
+#include <string.h>
+
+/* Private variables ---------------------------------------------------------*/
+uint8_t rx_buff[RX_BUFF_SIZE];
+static uint16_t rx_head = 0;
+static uint16_t rx_tail = 0;
+static uint16_t rx_count = 0;
+
+/* Function implementations --------------------------------------------------*/
+void rx_buff_init(void) {
+    rx_head = 0;
+    rx_tail = 0;
+    rx_count = 0;
+    memset(rx_buff, 0, RX_BUFF_SIZE);
+}
+
+uint16_t rx_buff_write(const uint8_t *data, uint16_t length) {
+    uint16_t bytes_written = 0;
+    
+    while (bytes_written < length && rx_count < RX_BUFF_SIZE) {
+        rx_buff[rx_head] = data[bytes_written];
+        rx_head = (rx_head + 1) % RX_BUFF_SIZE;
+        rx_count++;
+        bytes_written++;
+    }
+    
+    return bytes_written;
+}
+
+uint16_t rx_buff_read(uint8_t *data, uint16_t length) {
+    uint16_t bytes_read = 0;
+    
+    while (bytes_read < length && rx_count > 0) {
+        data[bytes_read] = rx_buff[rx_tail];
+        rx_tail = (rx_tail + 1) % RX_BUFF_SIZE;
+        rx_count--;
+        bytes_read++;
+    }
+    
+    return bytes_read;
+}
+
+void rx_buff_clear(void) {
+    rx_head = 0;
+    rx_tail = 0;
+    rx_count = 0;
+    memset(rx_buff, 0, RX_BUFF_SIZE);
+}
+
+uint16_t rx_buff_available(void) {
+    return rx_count;
+}
