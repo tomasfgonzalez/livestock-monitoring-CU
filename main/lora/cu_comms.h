cbe755a6aa9518f54aa5ac00655ef5dd817455a2
Brimix <only.brimix@gmail.com>
2025-06-01 01:56:32 -0300

    Add time since boot to send the LSU

--- main/lora/cu_comms.h
+++ main/lora/cu_comms.h
@@ -31,6 +31,7 @@
 typedef struct {
   uint32_t lsu_id;        /**< The ID assigned to the LSU */
   uint32_t period_ms;     /**< The period of time (in ms) in which the LSU will send data */
+  uint32_t now_ms;        /**< The current time (in ms) inside the period */
   uint32_t time_slot_ms;  /**< The time slot (in ms) assigned to the LSU for transmission within the period */
 } LSU_config_package_t;
 
